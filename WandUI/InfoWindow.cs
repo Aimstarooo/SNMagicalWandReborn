d17cd8311fb88eb70661ca0ba3f7c1ffba6ff90c
Aimstarooo <aimstarxd69@gmail.com>
2024-05-14 22:02:56 +0200

    InfoWindow.cs

--- WandUI/InfoWindow.cs
+++ WandUI/InfoWindow.cs
@@ -10,11 +10,11 @@ namespace SNMagicalWand.WandUI
         {
             GUI.color = Color.black;
             WandUIBase.Window("SNMagicalWand", new Rect(0, 10, 200, 100));
-            GUI.Label(new Rect(10, 25, 200, 100), "<color=gray>v.2.1 by xcar</color>");
+            GUI.Label(new Rect(10, 25, 200, 100), "<color=gray>v.2.1 by Aimstar</color>");
 
-            GUI.Label(new Rect(10, 45, 100, 100), "<color=yellow>[F1]</color> - <color=cyan>FullBright</color>");
-            GUI.Label(new Rect(10, 65, 200, 100), "<color=yellow>[Insert]</color> - <color=cyan>HideCheat</color>");
-            GUI.Label(new Rect(10, 85, 200, 100), "<color=yellow>[F2]</color> - <color=cyan>HideUI</color>");
+            GUI.Label(new Rect(10, 45, 100, 100), "<color=blue>[F1]</color> - <color=red>WidacWhuj</color>");
+            GUI.Label(new Rect(10, 65, 200, 100), "<color=blue>[Insert]</color> - <color=red>OffajTeCheaty</color>");
+            GUI.Label(new Rect(10, 85, 200, 100), "<color=blue>[F2]</color> - <color=red>SchowajUI</color>");
         }
     }
 }
